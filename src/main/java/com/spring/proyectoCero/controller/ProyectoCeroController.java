403f771a9ffdaef252505145f80b9615f455fc6c
Sergio Sacristan <>
2019-11-30 13:38:09 +0100

    Proyecto Spring MVC - Estructura basica + Controller

---
+++ src/main/java/com/spring/proyectoCero/controller/ProyectoCeroController.java
@@ -0,0 +1,13 @@
+package com.spring.proyectoCero.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("ProyectoCeroController")
+public class ProyectoCeroController {
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String hola(){
+		return "hola";
+	}
+}
