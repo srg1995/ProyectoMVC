7d50bc138cedae6a9dabaafd8aee747e27d617fd
Sergio Sacristan <>
2019-12-02 18:15:19 +0100

    Proyecto Spring MVC - modificaciones Controller

--- src/main/java/com/spring/proyectoCero/controller/ProyectoCeroController.java
+++ src/main/java/com/spring/proyectoCero/controller/ProyectoCeroController.java
@@ -1,8 +1,14 @@
 package com.spring.proyectoCero.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.spring.proyectoCero.dto.Dato;
 
 @Controller("ProyectoCeroController")
 public class ProyectoCeroController {
@@ -11,4 +17,24 @@ public class ProyectoCeroController {
 	public String cargaInicial(){
 		return "saludo";
 	}
+	
+	@RequestMapping(value = "/test/body", method = RequestMethod.GET)
+	@ResponseBody
+	public String obtenerParametro(@RequestParam(value = "dato") String dato) {
+	    return dato;
+	}
+	
+	@RequestMapping(value = "/test/bodyjson", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String obtenerJson(@RequestBody Dato dato) {
+	    return dato.getNombre();
+	}
+	
+	@RequestMapping(value = "/test/bodyjson", method = RequestMethod.POST)
+	@ResponseBody
+	public String obtenerTextoPost(@RequestBody String dato) {
+	    return dato;
+	}
+	
+	
 }
