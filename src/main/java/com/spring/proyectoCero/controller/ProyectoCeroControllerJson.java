fcd4e03436d903e1f8a9104bf6cf5ff7e5bfc3cb
ssacriss <ssacriss@everis.com>
2019-12-05 08:05:01 +0100

    Proyecto Spring MVC - modificaciones en la configuracion

---
+++ src/main/java/com/spring/proyectoCero/controller/ProyectoCeroControllerJson.java
@@ -0,0 +1,21 @@
+package com.spring.proyectoCero.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.spring.proyectoCero.dto.Dato;
+
+@Controller("ProyectoCeroJsonController")
+public class ProyectoCeroControllerJson {
+
+	
+	@RequestMapping(value = "/test/bodyjson", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String obtenerJson(@RequestBody Dato dato) {
+	    return dato.getNombre();
+	}
+}
